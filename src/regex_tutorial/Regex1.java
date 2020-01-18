931bb1ca9cdb6f377158c9b8d31ade744bb13a4a
Seongjin <seongjinkim1123@gmail.com>
2020-01-18 18:49:00 +0900

    20200118

--- src/regex_tutorial/Regex1.java
+++ src/regex_tutorial/Regex1.java
@@ -6,11 +6,11 @@ public class Regex1 {
 
 	public static void main(String[]agrs) {
 		
-		String longString = " Derec Banas CA 12345 PA (412)555-1212 johnsmith@hotmail.com 412-555-1234 412 555 1234";
+		String longString = " Derec Banas CA 12345 PA (412)555-1212 1-(412)555-1212 john._%-smith@hotmail.com 412-555-1234 412 555 1234";
 		String strangeString = " 1Z aaa **** *** {{{ {{ { ";
 		
-		System.out.println("\r 1 : 단어 찾기");
-		regexChecker("\\s[A-Za-z]{2,20}\\s", longString);  
+//		System.out.println("\r 1 : 단어 찾기");
+//		regexChecker("\\s[A-Za-z]{2,20}\\s", longString);  
 		//__ Derec, CA, PA 출력함  / Banas 는 Derec 체크 후 단어 앞 " " 가  없어져 Banas\\s 로 인식되므로 출력 X
 
 			//		[A-Za-f] - Any characters (alphabet)
@@ -26,8 +26,8 @@ public class Regex1 {
 			
 		
 	
-		System.out.println("\r 2 : 5자리 숫자 찾기 (zipcode)");	
-		regexChecker("\\s\\d{5}\\s", longString);
+//		System.out.println("\r 2 : 5자리 숫자 찾기 (zipcode)");	
+//		regexChecker("\\s\\d{5}\\s", longString);
 		//__ 12345 출력         | zipcode 가 integer 5 자리라고 가정하면 체크할 수 있음
 		
 			//   		\\d  : digit
@@ -35,40 +35,56 @@ public class Regex1 {
 			//			{5}  : length = 5
 			
 		
-		System.out.println("\r3 : 주 이름 찾기 (미국)");		
-		regexChecker("A[KLRZ]|C[AOT]", longString);
+//		System.out.println("\r3 : 주 이름 찾기 (미국)");		
+//		regexChecker("A[KLRZ]|C[AOT]", longString);
 			// 2 characters that start with a C or A 
 			// A[KLRZ]|C[ADT]
 		
 	
 		
-		System.out.println("\r4 : \" { \" 찾기");
+//		System.out.println("\r4 : \" { \" 찾기");
 		//regexChecker("(\\{{1,})", strangeString);
 				// 위 아래 같음.
-		regexChecker("(\\{+)", strangeString);  //__ {{{  , {{ , { 출력
+//		regexChecker("(\\{+)", strangeString);  //__ {{{  , {{ , { 출력
 		
-			// 	{5,}  : minimum length = 5 / maximum length = none ( 띄어쓰기 유의 ) 
-			//  +   :  최소 한번 이상 proceed 하라는 지시 ({1,} 와 같음)
-			//  - ^ * + ? {} [] \ | ()   :   
-			// 	  => 위 기호들은 regex 예약어 이므로 "\" back slash 를 앞에 써주어야 해당 문자 검색 가능
-		
-		
-		
-		System.out.println("\r5 : . 사용");
-		regexChecker(".{3}", strangeString); //__ 길이 세개 연속인 아무 문자	찾기 (white space 도 문자로 인식하는것 유의하기)
-		
-			// .  :  anything
+			// 		{5,}  : minimum length = 5 / maximum length = none ( 띄어쓰기 유의 ) 
+			//  	+   :  최소 한번 이상 proceed 하라는 지시 ({1,} 와 같음)
+			//  	- ^ * + ? {} [] \ | ()   :  
+					// ^ : caret
+			// 	  	=> 위 기호들은 regex 예약어 이므로 "\\" back slash 2개를 앞에 써주어야 해당 문자 검색 가능
 		
 		
-		System.out.println("\r6 : w 사용");
 		
+//		System.out.println("\r5 : . 사용");
+//		regexChecker(".{3}", strangeString); //__ 길이 세개 연속인 아무 문자	찾기 (white space 도 문자로 인식하는것 유의하기)
+		
+			// 		.  :  anything
+		
+		
+//		System.out.println("\r6 : w 사용");
+//		regexChecker("\\w*", strangeString);   //__ white space 포함한 모든 단어 출력
+		
+			// 		\\w  : any type of word 검색   = [A-Za-z0-9_] (동일)
+			// 		\\W  : not \\w
+			//		\\w*  : any type of word 중 길이 0 또는 0 이상인 모든 것 검색
+			//  	\\
+		
+		
+		System.out.println("\r7 : email 주소 찾기");
+		regexChecker("[A-Za-z0-9._%-]+@[A-Za-z0-9._-]+\\.[A-Za-z]{2,4}", longString);
+		// johnsmith@gmail.com
+		
+		
+		
+		System.out.println("\r8 : 다양한 타입의 phone number 찾기");
+		regexChecker("([0-9]{1,2}\\-)", longString);
+		// 1-(412)555-1212  (412)555-1212  412-555-1234  412 555-1234
 		
 	}
 	
 	public static void regexChecker(String theRegex, String str2Check) {
 		
 		Pattern checkRegex = Pattern.compile(theRegex);
-		
 		Matcher regexMatcher = checkRegex.matcher(str2Check);
 		
 		while(regexMatcher.find()) {
@@ -82,3 +98,14 @@ public class Regex1 {
 		
 	}
 }
+
+
+
+
+/*
+ * 질문 : 
+ * 1. meaning of _ and -
+ * 2. 9:43 : What are the parenthesis for in "(\\{+)" ?
+ * 3. "." seems to work either way of "." and "\\.". Am I correct?
+ * 4. 13:54  : Earlier in the video, "\\s" was referred as white space but can you also use " " for it?
+ * */
